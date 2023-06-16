35f400047130e775dee7a099c48bb147191019b6
Faizan <faizanalam980@gmail.com>
2023-06-16 17:26:37 +0530

    designed login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import '@passageidentity/passage-elements/passage-auth'
+import '../styles/login.css'
 
 function Login() {
 
     const apiKey = import.meta.env.VITE_REACT_APP_PASSAGE_APP_ID;
 
   return (
-    <passage-auth app-id={apiKey}></passage-auth>
+    <div className='container'>
+      <passage-auth app-id={apiKey} ></passage-auth>
+    </div>
   )
 }
 
